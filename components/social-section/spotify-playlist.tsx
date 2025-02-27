cb763b7bc26692cd304c0859a3f1d149b6301e8f
adictonator <adityabhaskarsharma@gmail.com>
2025-02-27 14:37:35 +0530

    added comp for showing my spotify playlists in a nice UI - better code consistency - cleaner UI

--- components/social-section/spotify-playlist.tsx
+++ components/social-section/spotify-playlist.tsx
@@ -1,23 +1,45 @@
-export default function SpotifyPlaylist() {
+import { SpotifyPlaylistProps } from '@/data/socials'
+import { motion } from 'framer-motion'
+import { ArrowUpRight } from 'lucide-react'
+import Link from 'next/link'
+
+export default function SpotifyPlaylist({
+	playlists,
+}: {
+	playlists: SpotifyPlaylistProps[]
+}) {
 	return (
-		<div className="space-y-2">
-			{platform.playlists.map(playlist => (
-				<div
-					key={playlist.name}
-					className="flex justify-between border border-neutral-800 bg-neutral-900 p-4">
-					<div>
-						<div className="text-sm text-neutral-300">
-							{playlist.name}
+		<ul className="mb-3.5">
+			{playlists.map((playlist, index) => (
+				<motion.li
+					key={index}
+					initial={{ opacity: 0, x: -20 }}
+					animate={{ opacity: 1, x: 0 }}
+					transition={{ delay: index * 0.1 }}
+					className="group/item relative w-full text-left">
+					<Link
+						href={playlist.url}
+						rel="noopener noreferrer"
+						target="_blank"
+						className="group/item hover:bg-secondary hover:border-border relative flex w-full items-start justify-between border-y border-dashed border-transparent px-6 py-4 transition-all duration-200">
+						<div>
+							<div className="text-sm text-neutral-300">
+								{playlist.name}
+							</div>
+							<div className="mt-1 text-xs text-neutral-500">
+								{playlist.tracks} tracks
+							</div>
 						</div>
-						<div className="mt-1 text-xs text-neutral-500">
-							{playlist.tracks} tracks
+						<div className="self-end text-xs text-neutral-500">
+							{playlist.followers} followers
 						</div>
-					</div>
-					<div className="text-xs text-neutral-500">
-						{playlist.followers} followers
-					</div>
-				</div>
+						<ArrowUpRight
+							className="text-primary absolute top-2.5 right-6 size-3.5 opacity-0 duration-300 group-hover/item:opacity-100"
+							strokeWidth={1}
+						/>
+					</Link>
+				</motion.li>
 			))}
-		</div>
+		</ul>
 	)
 }
--- data/socials.ts
+++ data/socials.ts
@@ -9,6 +9,7 @@ import PayPal from '@/components/social-icons/paypal'
 import Patreon from '@/components/social-icons/patreon'
 import Ko from '@/components/social-icons/ko-fi'
 import { SVGProps } from 'react'
+import LinkedIn from '@/components/social-icons/linked-in'
 
 // Define a type for donation platforms
 type DonationPlatform = {
@@ -45,7 +46,7 @@ export const socialPlatforms = [
 		stats: '100',
 		statLabel: 'Thriends?',
 		url: 'https://threads.net/@adictonator',
-		color: 'group-hover:from-neutral-900 group-hover:to-neutral-600',
+		color: 'group-hover/social:text-neutral-600',
 	},
 
 	{
@@ -55,7 +56,16 @@ export const socialPlatforms = [
 		stats: '300',
 		statLabel: 'Followers',
 		url: 'https://twitter.com/@adictonator',
-		color: 'group-hover:from-sky-500 group-hover:to-sky-600',
+		color: 'group-hover/social:text-neutral-600',
+	},
+	{
+		name: 'LinkedIn',
+		icon: LinkedIn,
+		handle: '-abs-',
+		stats: '350',
+		statLabel: 'Connections',
+		url: 'https://linkedin.com/in/-abs-',
+		color: 'group-hover/social:text-sky-600',
 	},
 	{
 		name: 'YouTube',
@@ -64,7 +74,7 @@ export const socialPlatforms = [
 		stats: '120',
 		statLabel: 'Subscribers',
 		url: 'https://youtube.com/@lcl',
-		color: 'group-hover:from-red-600 group-hover:to-red-700',
+		color: 'group-hover/social:text-red-700',
 	},
 	{
 		name: 'Instagram',
@@ -73,36 +83,42 @@ export const socialPlatforms = [
 		stats: '200',
 		statLabel: 'Followers',
 		url: 'https://instagram.com/lazycodelab',
-		color: 'group-hover:from-fuchsia-600 group-hover:to-orange-600',
+		color: 'group-hover/social:text-fuchsia-600',
 	},
 ]
+
+export interface SpotifyPlaylistProps {
+	name: string
+	url: string
+	tracks: string
+	followers: number
+}
+
+//export interface SpotifyPlaylist {
+//	name: string
+//	url: string
+//	tracks: string
+//	followers: number
+//}
+
+//export interface SpotifyPlaylistProps {
+//	playlists: SpotifyPlaylist[]
+//}
 
 export const misc = [
 	{
 		name: 'Discord',
 		icon: Discord,
 		handle: '@yourusername',
-		stats: '3.2K',
+		stats: '3',
 		statLabel: 'Members',
-		url: 'https://discord.gg/yourinvite',
-		color: 'group-hover:from-indigo-600 group-hover:to-indigo-700',
-		serverInfo: {
-			name: 'Dev Community',
-			status: 'online',
-			channels: [
-				{ name: '💻 coding-help', members: 42 },
-				{ name: '🎮 gaming', members: 28 },
-				{ name: '🎵 music-corner', members: 15 },
-			],
-			activeMembers: 156,
-			boostLevel: 2,
-		},
+		color: 'group-group-hover/social:from-indigo-600 group-group-hover/social:to-indigo-700',
 	},
 	//{
 	//	name: 'Buy Me a Coffee',
 	//	icon: BuyMeCoffee,
 	//	url: 'https://www.buymeacoffee.com/yourusername',
-	//	color: 'group-hover:from-yellow-500 group-hover:to-yellow-600',
+	//	color: 'group-group-hover/social:from-yellow-500 group-group-hover/social:to-yellow-600',
 	//	isDonation: true,
 	//	description: 'Support my work with a coffee or two',
 	//	stats: '52',
@@ -117,7 +133,7 @@ export const misc = [
 	//	name: 'PayPal',
 	//	icon: PayPal,
 	//	url: 'https://paypal.me/yourusername',
-	//	color: 'group-hover:from-blue-600 group-hover:to-blue-700',
+	//	color: 'group-group-hover/social:from-blue-600 group-group-hover/social:to-blue-700',
 	//	isDonation: true,
 	//	description: 'One-time or recurring donations via PayPal',
 	//},
@@ -125,7 +141,7 @@ export const misc = [
 	//	name: 'Patreon',
 	//	icon: Patreon,
 	//	url: 'https://patreon.com/yourusername',
-	//	color: 'group-hover:from-orange-600 group-hover:to-orange-700',
+	//	color: 'group-group-hover/social:from-orange-600 group-group-hover/social:to-orange-700',
 	//	isDonation: true,
 	//	stats: '18',
 	//	statLabel: 'Patrons',
@@ -151,7 +167,7 @@ export const misc = [
 	//	name: 'Ko-fi',
 	//	icon: Ko,
 	//	url: 'https://ko-fi.com/yourusername',
-	//	color: 'group-hover:from-sky-500 group-hover:to-sky-600',
+	//	color: 'group-group-hover/social:from-sky-500 group-group-hover/social:to-sky-600',
 	//	isDonation: true,
 	//	description: 'Support me with a Ko-fi donation',
 	//	stats: '34',
@@ -160,19 +176,33 @@ export const misc = [
 	{
 		name: 'Spotify',
 		icon: Spotify,
-		url: 'https://open.spotify.com/user/yourusername',
-		color: 'group-hover:from-emerald-600 group-hover:to-emerald-700',
+		url: 'https://open.spotify.com/user/lvkvexq7bztp62prc1w1j2gn7',
+		color: 'group-hover/social:text-emerald-600',
 		playlists: [
-			{ name: 'Coding Mode', followers: '2.3K', tracks: 42 },
 			{
-				name: 'Late Night Commits',
-				followers: '1.8K',
-				tracks: 35,
+				name: 'stabilizers',
+				followers: 3,
+				tracks: '250+',
+				url: 'https://open.spotify.com/playlist/0bqEPoeBZsdUtejKYqSXl0?si=af2bb77a39c049f1',
 			},
-		],
-		nowPlaying: {
-			track: 'Midnight Coding',
-			artist: 'Lo-Fi Developers',
-		},
+			{
+				name: 'the end',
+				followers: 10,
+				tracks: '150+',
+				url: 'https://open.spotify.com/playlist/4KMuon88haDvsEoxNCtGGy?si=f9a4e12faa694717',
+			},
+			{
+				name: 'yearning',
+				followers: 4,
+				tracks: '150+',
+				url: 'https://open.spotify.com/playlist/3mClFbeKwcIhqhGuy3PMsj?si=86b1441f5b5144c0',
+			},
+			{
+				name: 'bollytalgia',
+				followers: 1,
+				tracks: '100+',
+				url: 'https://open.spotify.com/playlist/0NZIL7tcSPaSqScxt3mDVo?si=049c3eedd6c646c7',
+			},
+		] as SpotifyPlaylistProps[],
 	},
 ]
